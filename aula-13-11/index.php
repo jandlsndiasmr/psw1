9fbcd788fde3318e3c3d3010f8d330edd10369ec
Jandilson Dias Moura <jandlsndiasmr@gmail.com>
2024-11-18 20:12:58 -0300

    ok

---
+++ aula-13-11/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula-13-11</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="links">
+        <nav>
+            <ul>
+                <li>
+                    <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/buffet/src/index.php">Buffet</a>
+                </li>
+                <li>
+                    <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/cineview/cineview/index.php">Cineview</a>
+                </li>
+                <li>
+                    <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/livraria/">Livraria</a>
+                </li>
+                <li>
+                    <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/neurobiblioteca/">NeuroBiblioteca</a>
+                </li>
+                <li>
+                    <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/sisdoacao/">Sis-doação</a>
+                </li>
+                <li>
+                    <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/sisgravadora/funcoes/login.php">Sis-gravadora</a>
+                </li>
+                <li>
+                    <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/sispapelaria/">Sis-papelaria</a>
+                </li>
+                <li>
+                    <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2bi/sisesporte/esporte/">Sis-esporte</a>
+                </li>
+                <li>
+                    <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2bi/sisrestaurante/">Sis-restaurante</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
