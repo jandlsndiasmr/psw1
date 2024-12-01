a2789bb1f9a01bc6f9b6493de090f403adc1af8b
Jandilson Dias Moura <jandlsndiasmr@gmail.com>
2024-12-01 14:55:10 -0300

    ok

---
+++ jandilson-atv-03/cadaluno.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <p>Nesta pagina colocar cabeçalho, rodape e criar um formulario com os seguintes atributos.</p>
+    <p>nome, endereco, bairro, cidade, UF, cpf, celular, zap, datanascimento, disciplinascursadas, disciplinaspreferidas.   </p>
+    <p> Linkar com CSS</p> 
+    <p>codigo para fazer o formulario</p>
+    <p>https://www.w3schools.com/css/css_form.asp</p>
+</body>
+</html>
---
+++ jandilson-atv-03/readme.md
@@ -0,0 +1,6 @@
+# atividade-10-12
+ 
+ Atividade para entregar  dia 10-12
+ Fazer todo conteudo utilizando apenas w3schools
+ valor 3 pontos
+ tem que seguir o modelo feito em sala de aula.
